54abb430e9a9c69673a753101d77bd9424103907
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-04-22 09:35:56 +0200

    Mise en fonction des boutons caroussel "précédent et suivant" sur la page catégorie + Création d'une paje javascript pour le caroussel

---
+++ Evaluation_diw_lignerouge/lignerouge.js
@@ -0,0 +1,12 @@
+/*          ****************** Mes boutons carousel précédent et suivant sur la page catégorie  *************************   */
+function precedent()
+{
+document.getElementById("carouselcatprec").click();
+}
+
+function suivant()
+{
+document.getElementById("carouselcatsuiv").click();
+}
+
+/*          ****************** ICI                *************************   */
