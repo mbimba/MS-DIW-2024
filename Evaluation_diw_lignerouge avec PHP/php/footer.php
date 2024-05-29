fbfbb29fd8ccbb00ef436098aa88e6a39bd14b45
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-05-29 16:14:58 +0200

    supression des liens bootstrap et javascript  et ajout dans mon footer et header.

--- Evaluation_diw_lignerouge avec PHP/php/footer.php
+++ Evaluation_diw_lignerouge avec PHP/php/footer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Footer</title>
-</head>
-<body>
-   
+
 <!--         ICI MON PIED DE PAGE AVEC LES LOGOS    -->
 
     
@@ -65,38 +57,11 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+  integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+  crossorigin="anonymous"></script>
+  <script src="lignerouge.js"></script>
 
 
 
