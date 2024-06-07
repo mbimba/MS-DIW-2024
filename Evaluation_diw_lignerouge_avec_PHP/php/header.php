fcf9d0090312800fd633cac3f0d07ce00cfd0a41
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-06-07 08:40:59 +0200

    Modification du nom de ma page accueil.php en index.php et modif sur la page header. Ajout d'un href à mon logo dans header qui renvoie à la page accuei à chaque clic.

--- Evaluation_diw_lignerouge_avec_PHP/php/header.php
+++ Evaluation_diw_lignerouge_avec_PHP/php/header.php
@@ -14,12 +14,14 @@
       <!--        NAVBAR ENTETE         SANS L'ONGLET DE RECHERCHE      -->
       <nav class="navbar navbar-expand-sm navbar-dark bg-dark fs-2  fixed-top">
         <div class="container-fluid-flex ">
+          <a href="index.php">
           <img src="images_the_district/the_district_brand/facebook_cover_photo_1.png" class="object-fit-contain border rounded" width="80" alt="">
+          </a>
         </div>
         <div class="container-fluid d-flex justify-content-evenly " >
           <ul class="navbar-nav">
             <li class="nav-item col-sm-4 ">
-              <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/Accueil.php") {echo "active";}?> espace" href="Accueil.php">Accueil</a>
+              <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/index.php") {echo "active";}?> espace" href="index.php">Accueil</a>
             </li>
             <li class="nav-item col-sm-4 "> 
               <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/Cat%C3%A9gorie.php"||$_SERVER['REQUEST_URI'] == "/Platcategorieasiatique.php"||$_SERVER['REQUEST_URI'] == "/Platcategorieburger.php" ||$_SERVER['REQUEST_URI'] == "/Platcategoriegrillade.php" ||$_SERVER['REQUEST_URI'] == "/Platcategoriepate.php" ||$_SERVER['REQUEST_URI'] == "/Platcategoriepizza.php" ||$_SERVER['REQUEST_URI'] == "/Platcategoriesalade.php" ||$_SERVER['REQUEST_URI'] == "/Platcategoriesandwich.php" ||$_SERVER['REQUEST_URI'] == "/Platcategorievegan.php") {echo "active";}?>  espace" href="Catégorie.php">Catégorie</a>
