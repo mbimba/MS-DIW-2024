02dd50603b7382cce2a642ebf8e767598d54644c
MBIMBA Triomphe <mbimbatriomphe@gmail.com>
2024-06-07 09:10:10 +0200

    Exo mot de passe 2: suite

--- TEST_PHP/ncode-exolesmotsdepasse2.php
+++ TEST_PHP/ncode-exolesmotsdepasse2.php
@@ -8,12 +8,18 @@
 <body>
 <h2> EXERCICE 2 PHP: MOT DE PASSE </h2>
 <p>
-    <strong> 
+    <strong> Reprendre l'exercice précédent et y ajouter la gestion des mots de passe selon les consignes suivantes :
     </strong>
 </p>
-   
 
+<p>
 
+Créer une table user destinée à stocker le nom, le prénom, l'adresse mail, le mot de passe. <br>
+    Créer un formulaire d'inscription pour renseigner et enregistrer les informations demandées. Celles-ci doivent être vérifiées correctement avant d'être enregistrées. <br>
+    Modifier le script PHP d'authentification pour vérifier le login et le mot de passe par rapport au contenu de la table user.<br>
+    Vous devrez utiliser les fonctions PHP password_hash() et password_verify().
+
+</p>
 
 
 
